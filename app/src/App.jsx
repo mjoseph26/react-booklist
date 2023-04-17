27a68e6b19deced9821bab3f5c6bae704ee3e6c2
mjoseph26 <mathew.joseph@xandertalent.com>
2023-04-17 16:15:16 +0100

    Added the ability to get book details upon button click

--- app/src/App.jsx
+++ app/src/App.jsx
@@ -9,23 +9,21 @@ import bookArray from './books';
 
 
 function App() {
-  
-  const getBook = bookArray.find(id => {
-    id === bookArray.id;
-  });
-  console.log(getBook);
-
-
+  const getBook = (id) => {
+    const book = bookArray.find(book => book.id === id);
+    console.log(book);
+  };
   return (
     <section className = 'booklist'>
       <EventExample />
       {bookArray.map((book) => {
         const {img, title, author, id} = book;
         //console.log(img, title, author)
-        return <Book key={id} img={img} title={title} author={author} getBook={getBook} />;
+        return <Book key={id} id={id} img={img} title={title} author={author} getBook={getBook} />;
         }
       )};
     </section>
   );
+  
 }
 export default App;
--- app/src/book.jsx
+++ app/src/book.jsx
@@ -1,12 +1,14 @@
-function Book({img, title, author, children}) {
-    return (
-      <article className="book">
-        <img src={img} alt={title} />
-        <h2>{title}</h2>
-        <h4>{author}</h4>
-        {children}
-      </article>
-    );
-  }
+// Contains our Book component
+function Book({ id, img, title, author, getBook, children }) {
+  return (
+    <article className="book">
+      <img src={img} alt={title} />
+      <h2>{title}</h2>
+      <h4>{author}</h4>
+      {children}
+      <button className="btn" onClick={() => getBook(id)}>Get Book</button>
+    </article>
+  );
+}
 
 export default Book;
